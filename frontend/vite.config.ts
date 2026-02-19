3b0ecd4431d80f0ca3d5094a31ff24c129153742
Trixploit <nikitik81@gmail.com>
2026-02-19 10:13:40 +0300

    feat(frontend): replace old stack with vite, tailwind and shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [tailwindcss(), react()],
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
+   },
+   host: true,
 });
