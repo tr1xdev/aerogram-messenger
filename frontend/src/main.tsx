ad128937b993aa158f1ab60736f556b8ef2b986d
Trixploit <nikitik81@gmail.com>
2026-02-23 05:11:15 +0300

    fix(docs): move .example.env to backend dir

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,17 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -12,5 +20,7 @@ declare module "@tanstack/react-router" {
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
 );
