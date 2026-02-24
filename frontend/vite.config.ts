ff1f50b22743691406d802815da599dad0cacc6f
Trixploit <nikitik81@gmail.com>
2026-02-24 11:26:13 +0300

    feat: add tanstack router vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,18 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({
+      routesDirectory: "./src/app/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      quoteStyle: "single",
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
