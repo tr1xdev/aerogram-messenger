cb0261a58a9f5e2511b755bca734f02145f7adb8
Trixploit <nikitik81@gmail.com>
2026-02-26 04:07:20 +0300

    feat(frotnend): add user store

---
+++ frontend/src/store/user.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import type { User } from "@/entities/chat/model/types";
+
+interface UserState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}));
