604e5ef0555d7c7357bb078134b12a1ef80c1480
Trixploit <nikitik81@gmail.com>
2026-03-03 10:44:04 +0300

    feat(user): add security fields to user schema and models

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -19,6 +19,8 @@ type User struct {
 	VerificationToken  string `gorm:"type:text"`
 	VerificationExpiry time.Time
 	PublicKey          *string        `gorm:"type:text"`
+	EncryptedPrivKey   *string        `gorm:"type:text"`
+	EncryptionIv       *string        `gorm:"type:text"`
 	CreatedAt          time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt          time.Time      `gorm:"autoUpdateTime"`
 	DeletedAt          gorm.DeletedAt `gorm:"index"`
