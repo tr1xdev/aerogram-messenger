03ae496582c353a8dd5c25e6e5ba64131c9964e2
Trixploit <nikitik81@gmail.com>
2026-03-16 18:26:27 +0300

    feat: minor improvments

--- backend/internal/graph/resolvers/message.go
+++ backend/internal/graph/resolvers/message.go
@@ -284,23 +284,3 @@ func (r *subscriptionResolver) DialogRead(ctx context.Context, chatID string) (<
 func (r *Resolver) Message() graph.MessageResolver { return &messageResolver{r} }
 
 type messageResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *messageResolver) ReplyTo(ctx context.Context, obj *model.Message) (*model.Message, error) {
-	if obj == nil || obj.ReplyTo == nil || obj.ReplyTo.ID == "" {
-		return nil, nil
-	}
-
-	if obj.ReplyTo.Text != "" {
-		return obj.ReplyTo, nil
-	}
-
-	return loaders.LoadMessage(ctx, obj.ReplyTo.ID)
-}
-*/
