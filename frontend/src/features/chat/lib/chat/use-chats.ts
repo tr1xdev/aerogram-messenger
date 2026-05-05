2fbe0856d4d7e4c9c66b3f7878d4c583a4ebfbd4
tr1xdev <55844996+tr1xdev@users.noreply.github.com>
2026-05-05 09:56:47 +0300

    feat(chat): update message actions logic and composer ui

--- frontend/src/features/chat/lib/chat/use-chats.ts
+++ frontend/src/features/chat/lib/chat/use-chats.ts
@@ -35,6 +35,7 @@ const chatDetailsQuery = graphql`
         ...useMarkDialog_chat
         ...chatMenuItem_chat
         id
+        slug
         title
         type
         photoUrl
@@ -44,6 +45,7 @@ const chatDetailsQuery = graphql`
         lastReadSequence
         myReadSequence
         canWrite
+        myRole
         permissions {
           canSendMessage
           canInviteUsers
@@ -82,7 +84,7 @@ export function useMyChats(): MyChatsResponse {
   return useLazyLoadQuery<useChatsMyChatsQuery>(
     myChatsQuery,
     {},
-    { fetchPolicy: "store-or-network" },
+    { fetchPolicy: "store-and-network" },
   );
 }
 
@@ -91,7 +93,7 @@ export function useChatDetails(chatId: string): ChatDetailsResponse {
     chatDetailsQuery,
     { id: chatId },
     {
-      fetchPolicy: "store-or-network",
+      fetchPolicy: "store-and-network",
       fetchKey: chatId,
     },
   );
